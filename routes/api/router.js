f3e6ec128c364275878b192d224091fde1ea6e2d
Chantsova Anastasia <stasyadiv@gmail.com>
2021-11-28 18:17:19 +0200

    Change validations path

--- routes/api/router.js
+++ routes/api/router.js
@@ -1,19 +1,9 @@
 const { Router } = require('express')
 const contactsController = require('../../controllers/controller.js')
 const { joiSchema, favoriteJoiSchema } = require('../../models/contact')
+const validation = require('../../helpers/validation')
 
 const router = Router()
-
-const validation = (schema) => {
-  return (req, res, next) => {
-    const { error } = schema.validate(req.body)
-    if (error) {
-      error.status = 400
-      next(error)
-    }
-    next()
-  }
-}
 
 router.get('/', contactsController.getAllContacts)
 router.get('/:contactId', contactsController.getContactById)
