19eb24802231df697e8718e813769186f0266875
Chantsova Anastasia <stasyadiv@gmail.com>
2021-12-03 11:22:39 +0200

    hw04-auth Base solutions
    hw04-auth Base solutions

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
+<<<<<<< Updated upstream
 const contactsRouter = require('./routes/api/contacts')
+=======
+const authRouter = require('./routes/api/auth')
+const contactsRouter = require('./routes/api/contacts')
+const usersRouter = require('./routes/api/users')
+>>>>>>> Stashed changes
 
 const app = express()
 
@@ -12,10 +18,12 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
+app.use('/api/auth', authRouter)
+app.use('/api/users', usersRouter)
 app.use('/api/contacts', contactsRouter)
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+  res.status(404).json({ message: 'Not found!' })
 })
 
 app.use((err, req, res, next) => {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const express = require('express')
 const router = express.Router()
 
@@ -20,5 +21,20 @@ router.delete('/:contactId', async (req, res, next) => {
 router.patch('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
+=======
+const { Router } = require('express')
+const { joiSchema, favoriteJoiSchema } = require('../../models/contact')
+const { ctrlWrapper, validation, auth } = require('../../middlewars')
+const { contacts: ctrl } = require('../../controllers')
+
+const router = Router()
+
+router.get('/', auth, ctrlWrapper(ctrl.getAllContacts))
+router.get('/:contactId', ctrlWrapper(ctrl.getContactById))
+router.post('/', auth, validation(joiSchema), ctrlWrapper(ctrl.addNewContact))
+router.delete('/:contactId', ctrlWrapper(ctrl.deleteContactById))
+router.put('/:contactId', validation(joiSchema), ctrlWrapper(ctrl.updateContactById))
+router.patch('/:contactId/favorite', validation(favoriteJoiSchema), ctrlWrapper(ctrl.updateStatusContact))
+>>>>>>> Stashed changes
 
 module.exports = router
